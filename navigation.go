cb95fca1483fbd46d11f6b61e909380dc30265c3
driver <driver.devel@gmail.com>
2015-04-24 22:21:04 +0300

    Extend navigation struct

--- navigation.go
+++ navigation.go
@@ -1,10 +1,20 @@
 package betfair
 
+import (
+	"encoding/json"
+	"time"
+)
+
 type NavigationChild struct {
-	Type     string `json:"type"`
-	Name     string `json:"name"`
-	ID       string `json:"id"`
-	Children []NavigationChild
+	Type            string          `json:"type"`
+	Name            string          `json:"name"`
+	ID              string          `json:"id"`
+	CountryCode     string          `json:"countryCode"`
+	ExchangeID      string          `json:"exchangeId"`
+	MarketType      string          `json:"marketType"`
+	MarketStartTime time.Time       `json:"marketStartTime"`
+	NumberOfWinners json.RawMessage `json:"numberOfWinners"`
+	Children        []NavigationChild
 }
 
 type Navigation struct {
